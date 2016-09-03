634db38c260c7f3ff7c131f91d64c0879385cdd6
Thomas Boudreaux <thomas@boudreauxmail.com>
2016-09-03 12:32:03 -0400

    Add files via upload

---
+++ InteractionController.py
@@ -0,0 +1,45 @@
+##############################
+#   Interaction Controller   #
+##############################
+"""
+Functionality: Stores the functions and classes to be run at individual world coords
+Dependencies: N/A
+References: WorldController.py
+"""
+
+
+def I_FUNC_0_0():
+    print 'This is the start of your adventure, godspeed'
+    return '/s/look_around(),/d/off'
+
+def I_FUNC_10_14():
+    print 'TEST'
+    return 0
+
+def I_FUNC_24_24():
+    print 'TEST'
+    return 0
+
+def I_FUNC_0_24():
+    print 'TEST'
+    return 0
+
+def I_FUNC_24_0():
+    print 'TEST'
+    return 0
+
+def I_FUNC_13_0():
+    print 'TEST'
+    return 0
+
+def I_FUNC_0_13():
+    print 'TEST'
+    return 0
+
+def I_FUNC_13_24():
+    print 'TEST'
+    return 0
+
+def I_FUNC_24_13():
+    print 'TEST'
+    return 0
